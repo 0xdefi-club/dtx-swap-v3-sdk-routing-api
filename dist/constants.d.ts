e59857de0cf9d34bf3d9ab0e8f28615a2904ef97
zj kkk <npckei217@gmail.com>
2024-07-01 11:18:53 +0800

    feat:update

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,18 +1,23 @@
-export declare const FACTORY_ADDRESS = '0xfaa89f95CE1948582154ee1C0Ea47701cBb95E04'
-export declare const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export declare const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+import { ChainId } from '@uniswap/sdk-core';
+export declare const FACTORY_ADDRESS = "0xfCA1AEf282A99390B62Ca8416a68F5747716260c";
+export declare const FACTORY_ADDRESS_MAP: {
+    84532: string;
+    167000: string;
+};
+export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export declare const POOL_INIT_CODE_HASH = "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
  */
 export declare enum FeeAmount {
-  LOWEST = 100,
-  LOW = 500,
-  MEDIUM = 3000,
-  HIGH = 10000
+    LOWEST = 100,
+    LOW = 500,
+    MEDIUM = 3000,
+    HIGH = 10000
 }
 /**
  * The default factory tick spacings by fee amount.
  */
 export declare const TICK_SPACINGS: {
-  [amount in FeeAmount]: number
-}
+    [amount in FeeAmount]: number;
+};
